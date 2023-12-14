ae54936c73189b65524adff45aff914bd2db7abc
thespielplatz <informatics@gmx.net>
2023-12-14 22:40:44 +0100

    Addresses & clear development mnemonic

--- README.md
+++ README.md
@@ -4,11 +4,12 @@
 
 ### Todos
 
-- [ ] Page 1: SeedWords with foldable back (quasi "Recovery Words" und "Restore" von BitBox02 Back card template) fürs laminieren
+- [x] Page 1: SeedWords with foldable back (quasi "Recovery Words" und "Restore" von BitBox02 Back card template) fürs laminieren
+- [x] Page 2: Foldable thingy like BitBox02 Backup Card, just that cutted and laminated Page 1 fits into this
+- [x] Page 3: Acount 0 XPUB with QR Code and Joe Text
+- [x] Page 4 till X: Addresses incl. QR Code
+- [ ] Loading indicater when stuff is calculated
 - [ ] Toggle for either Seed or Account
-- [ ] Page 2: Foldable thingy like BitBox02 Backup Card, just that cutted and laminated Page 1 fits into this
-- [ ] Page 3: Acount 0 XPUB with QR Code and Joe Text
-- [ ] Page 4 till X: Addresses incl. QR Code
 
 ###  Docs
 ```bash
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const seedGrid = document.getElementById('seed-grid')
+const ADDRESSES_PER_PAGE = 8
+
 const seedFieldTemplate = document.getElementById('template-seed-field').innerHTML
+const addressBoxTemplate = document.getElementById('template-address-box').innerHTML
+const addressSheetTemplate = document.getElementById('template-address-sheet').innerHTML
+
+const seedGrid = document.getElementById('seed-grid')
 const mnemonicInfo = document.getElementById('mnemonic-info')
 const mnemonicInput = document.getElementById('mnemonic-input')
 const acount0xpubField =document.getElementById('acount0xpub')
@@ -16,7 +21,11 @@ const zpubQRCode = new QRCode(document.getElementById('zpub-qrcode'), {
 zpubQRCode.clear()
 
 const wordFields = []
+let addressIndex = 0
+let account0Node
+
 const zeroPad = (num, places = 2) => String(num).padStart(places, '0')
+
 for (let i = 0; i < 24; ++i) {
   const div = document.createElement('div')
   div.innerHTML = seedFieldTemplate
@@ -73,10 +82,25 @@ const account2Address = (acountNode, index) => {
   }
 }
 
+const createAddressPage = () => {
+  const addressPage = document.createElement('section')
+  addressPage.setAttribute('class', 'sheet padding-10mm')
+  addressPage.setAttribute('data-address-page', 'true')
+  addressPage.innerHTML = addressSheetTemplate
+  document.getElementById('theBody').appendChild(addressPage)
+  return addressPage
+}
+
 const mnemonicChanged = async () => {
   acount0xpubField.innerText = ''
   acount0zpubField.innerText = ''
-  zpubQRCode.clear()
+  zpubQRCode.makeCode('notvalid')
+  account0Node = null
+  addressIndex = 0
+
+  document.querySelectorAll('[data-address-page=true]').forEach(element => {
+    element.remove()
+  })
 
   const mnemonic = mnemonicInput.value
 
@@ -92,7 +116,7 @@ const mnemonicChanged = async () => {
   if (!valid) return
 
   const bip39Seed = await bip39lib.mnemonicToSeed(mnemonic)
-  const bip39HEX= bip39Seed.toString('hex')
+  const bip39HEX = bip39Seed.toString('hex')
 
   const bip32 = bip32lib.BIP32Factory(ecc)
   const hdNode = bip32.fromSeed(bip39Seed)
@@ -107,7 +131,7 @@ const mnemonicChanged = async () => {
   const bip32ZPUB = pubToPub(bip32XPUB, 'zpub')
 
   const path = "m/84'/0'/0'"
-  const account0Node = hdNode.derivePath(path)
+  account0Node = hdNode.derivePath(path)
 
   const account0XPRV = account0Node.toBase58()
   const account0ZPRV = prvToPrv(account0XPRV, 'zprv')
@@ -119,40 +143,41 @@ const mnemonicChanged = async () => {
   acount0zpubField.innerText = account0ZPUB
   zpubQRCode.makeCode(account0ZPUB)
 
-  QRCode.toCanvas(zpubQRCode, account0ZPUB, function (error) {
-    if (error) console.error(error)
-  })
-
   //console.log('Account0 - ZPRV', account0ZPRV)
   //console.log('Account0 - ZPUB', account0ZPUB)
 
-  const { publicKey, privateKey, bc1Address } = account2Address(account0Node, 0)
-  console.log('Public Key', publicKey)
-  console.log('Private Key', privateKey)
-  console.log('bc1Address', bc1Address)
+  addAddressPage()
+}
 
+const addAddressPage = () => {
+  let addressGrid
+
+  for (let i = 0; i < ADDRESSES_PER_PAGE; ++i) {
+    if (i % ADDRESSES_PER_PAGE === 0) {
+      const addressPage = createAddressPage()
+      addressPage.getElementsByTagName('h1')[0].innerHTML = `Addresses from ${addressIndex} to ${addressIndex + ADDRESSES_PER_PAGE - 1}`
+      addressGrid = addressPage.getElementsByClassName('address-grid')[0]
+    }
+    const { bc1Address } = account2Address(account0Node, addressIndex)
+
+    const div = document.createElement('div')
+    div.innerHTML = addressBoxTemplate
+    addressGrid.appendChild(div)
+
+    div.getElementsByTagName('span')[0].innerText = `m/84'/0'/0'/0/${addressIndex}`
+    new QRCode(div.getElementsByClassName('address-qrcode')[0], {
+      text: bc1Address,
+      width: 175,
+      height: 175,
+    })
+    div.getElementsByClassName('address-bc1')[0].innerHTML = bc1Address
+
+    addressIndex++
+  }
 }
 
 mnemonicInput.addEventListener('keyup', mnemonicChanged)
+document.getElementById('btnAddAddressPage').addEventListener('click', addAddressPage)
 
 window.onload = () => {
-  mnemonicChanged()
 }
-
-/*
-inputQRCode.addEventListener("keyup", function(event) {
-  event.preventDefault()
-  console.log(inputQRCode.value)
-  qrcode.clear()
-  qrcode.makeCode(inputQRCode.value.trim())
-
-  textId.innerText = inputQRCode.value
-
-  const parts = inputQRCode.value.split('/')
-  const codeFull = parts.at(-1)
-  const codeParts = codeFull.split('-')
-  const codeMinimized = codeParts.at(0).toUpperCase()
-
-  minimizedCode.innerText = codeMinimized
-})
-*/
